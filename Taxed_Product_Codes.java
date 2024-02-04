cec9962c4896861fbf20126dc2f38995893ab1ce
MÜCAHİT YUŞA DEMİR <m.yusadmr@gmail.com>
2024-02-04 16:10:21 +0300

    Create Taxed_Product_Codes.java

---
+++ Taxed_Product_Codes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+public class Taxed_Product_Codes {
+    
+    public static void main(String[] args) {
+        
+        int tax_free;
+        double new_price;
+
+        Scanner input= new Scanner(System.in);
+
+        out.println("Enter the tax-free price of the product ?: ");
+        tax_free=input.nextInt();
+        
+        new_price = tax_free+ (tax_free * 0.18);
+        
+        out.println(" Taxed price of the product= " + new_price);
+    }
+}
+
